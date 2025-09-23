0f100e0bc673e50eedeef4dbe18e79e448077f5b
EentErt <nate.giff@gmail.com>
2025-09-23 15:54:59 -0700

    added getUsers(), need to implement handler

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { exit } from "process";
 import { Config } from "./config.js";
 import { setUser, readConfig } from "./config.js";
 import { db } from "./lib/db/index.js";
-import { createUser, getUserByName } from "./lib/db/queries/users.js";
+import { createUser, getUserByName, reset } from "./lib/db/queries/users.js";
 
 let cfg = {} as Config;
 
@@ -10,43 +10,61 @@ async function main() {
     let registry = {} as CommandsRegistry;
     registerCommand(registry, "login", handlerLogin);
     registerCommand(registry, "register", handlerRegister);
+    registerCommand(registry, "reset", handlerReset);
 
     cfg = readConfig();
 
     const args = process.argv.slice(2);
+    console.log("Running command:", args[0])
     try {
         await runCommand(registry, args[0], ...args.slice(1));
     } catch (error) {
         console.log("Error:", error);
-        exit(1);
+        process.exit(1);
     }
     process.exit(0);
 }
 
 type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
 
-async function handlerLogin(cmdName: string, ...args: string[]) {
-    if (args.length < 1) {
+async function handlerLogin(cmdName: string, ...args: string[]): Promise<void> {
+    if (args[0] === undefined) {
         throw new Error("username required"); 
+    }
+
+    console.log("Checking if user exists")
+    let existing = await getUserByName(args[0]);
+    if (existing === undefined) {
+        throw new Error(`No user with name ${args[0]} exists`)
     }
 
     setUser(cfg, args[0]);
     console.log("Logged in as", args[0]);
 }
 
-async function handlerRegister(cmdName: string, ...args: string[]) {
-    if (args.length < 1) {
+async function handlerRegister(cmdName: string, ...args: string[]): Promise<void> {
+    if (args[0] === undefined) {
         throw new Error("name required"); 
     }
-    try {
-        let existing = await getUserByName(args[0]);
-        if existing =
+
+    console.log("Checking if user exists")
+    let existing = await getUserByName(args[0]);
+    if (existing !== undefined) {
+        throw new Error(`User with name ${args[0]} already exists`)
     }
+
+    console.log("User does not exist. Creating user.")
     try {
         let user = await createUser(args[0]);
+        setUser(cfg, user.name);
+        console.log("Created and logged in as user:", args[0]);
     } catch (error) {
         throw error;
     }
+}
+
+async function handlerReset(cmdName: string, ...args: string[]): Promise<void> {
+    await reset();
 }
 
 type CommandsRegistry = Record<string, CommandHandler>;
@@ -55,13 +73,13 @@ function registerCommand(registry: CommandsRegistry, cmdName: string, handler: C
     registry[cmdName] = handler;
 }
 
-async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
+async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]): Promise<void> {
     const handler = registry[cmdName];
     if (!handler) {
         throw new Error("unknown command: " + cmdName);
     }
 
-    handler(cmdName, ...args);
+    await handler(cmdName, ...args);
 }
 
 
